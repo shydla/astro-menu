59f983920f3eeb58a56fc8b1e43a0351a326c435
Roman Samsonov <shydla@gmail.com>
2024-03-29 09:01:56 +0400

    fix: site off

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from '@astrojs/tailwind'
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://shydla.github.io',
-  base: 'astro-menu',
+  // site: 'https://shydla.github.io',
+  // base: 'astro-menu',
 })
